e7d70425fde00d3c71a3eb81022e959fcccc0334
kjwanders11 <kjwanders11@gmail.com>
2025-12-01 10:34:53 -0700

    Added print_menu to the main-app

--- main-app.py
+++ main-app.py
@@ -1,1 +1,12 @@
-print("This is my main code")
+def print_menu():
+    print("Welcome to our Final Project")
+    print("What would you like to do?")
+    print("1. Create a new project")
+    print("2. View projects")
+    print("3. Exit")
+    user_input = input("Enter your choice: ")
+    return user_input
+
+
+print("This is our main code")
+print_menu()
