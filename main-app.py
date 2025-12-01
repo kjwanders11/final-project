841f6e6e71ee04da27e975b8c7accabe5770a4a5
kjwanders11 <kjwanders11@gmail.com>
2025-12-01 10:43:07 -0700

    Added get_user_info function to main-app

--- main-app.py
+++ main-app.py
@@ -1,1 +1,7 @@
-print("This is my main code")
+def get_user_info():
+    user_name = input("Enter your name: ")
+    user_password = input("Enter your password: ")
+    return user_name, user_password
+
+print("This is my main project code")
+uname, upassword = get_user_info()
